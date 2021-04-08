eb1ce9c631608898514799b1106b70ad4d5eea3f
Peter Okerulu <oduchep@gmail.com>
2021-04-08 22:25:14 +0100

    my first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".mobile-menu-icons").addEventListener("click", () => {
+	document.querySelector(".nav-links").classList.toggle("main-nav");
+	document.querySelector(".nav-bar-mobile").classList.toggle("main-nav");
+});
